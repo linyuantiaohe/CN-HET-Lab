51cbaa2f86d85895498d23233c7595bcfde1afaa
Ge Wang <gewang_cupb@outlook.com>
2025-12-07 23:33:01 +0800

    更新

--- start.ps1
+++ start.ps1
@@ -1,4 +1,2 @@
-D:
-cd D:\Github\CN-HET-Lab
 conda activate model
-pythonw -m streamlit run .\HomePage.py --server.enableXsrfProtection=false --server.port 7000
+streamlit run .\HomePage.py --server.enableXsrfProtection=false --server.port 7000
